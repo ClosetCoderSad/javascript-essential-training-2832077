9170d84a2916de36b1e82b08c06c115ce16ee3d1
Md Abrar Al Zabir <abrarroll2@gmail.com>
2024-06-30 19:11:15 +0600

    Update script.js

--- Practice/03_07/script.js
+++ Practice/03_07/script.js
@@ -7,3 +7,15 @@
  * - Find an object that has another object inside of it to create a nested object.
  * - Test your objects in the browser console by accessing the entire object and its specific properties.
  */
+const myBackPack = {
+    volume: 40,
+    color: "grey",
+    pocketNum: 15,
+    contents:8,
+    strapLength:{
+        left: 25,
+        right: 25,
+    },
+    lidOpen: false,
+};
+
